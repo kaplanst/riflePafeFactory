e0f937070e23a29b0e6dda8b298935ab5beffa67
Tim Yoshi <tim.yoshi@gmail.com>
2021-11-29 21:48:34 -0500

    Carefully check Java version. It might change.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Хуй!");
+        System.out.print("Хуй! ");
+        System.out.print("Вам!");
+
     }
 }
