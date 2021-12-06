8407786c36507968e8a8a50f10c6d06b6d14f365
tim-yoshi <42818031+tim-yoshi@users.noreply.github.com>
2021-12-05 22:14:57 -0500

    Rewrote test to POM (#11)

--- src/test/java/Tim.java
+++ src/test/java/Tim.java
@@ -1,3 +1,4 @@
+import Utils.BaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,47 +8,26 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class Tim {
+public class Tim extends BaseTest {
 
     @Test
     void fairingsClickTest(){  // Test case #TC-MP-001
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://rifle.com/");
         driver.findElement(By.xpath("//*[@id=\"ctl00_ctl00_NestedMaster_PageContent_webpagePage\"]/div/div[1]/div[1]/a/img")).click();
         Assert.assertEquals(driver.getTitle(), "Fairings");
-        driver.quit();
     }
 
     @Test
     void fairingsTitleTest(){  // Test case #TC-MP-002
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://rifle.com/");
         driver.findElement(By.xpath("//*[@id=\"ctl00_ctl00_NestedMaster_PageContent_webpagePage\"]/div/div[1]/div[1]/a/img")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Fairings");
-        driver.quit();
     }
 
     @Test
     void fairingsSortTest(){  // Test case #TC-MP-003
-        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://rifle.com/");
         driver.findElement(By.xpath("//*[@id=\"ctl00_ctl00_NestedMaster_PageContent_webpagePage\"]/div/div[1]/div[1]/a/img")).click();
         driver.findElement(By.xpath("//*[@class='form-control-inline']")).click();
         driver.findElement(By.xpath("//*[@id=\"ctl00_ctl00_NestedMaster_PageContent_ctl00_SortResults\"]/option[3]")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"ctl00_ctl00_NestedMaster_PageContent_ctl00_CatalogNodeList_ctl00_CategoryItem_CategoryName\"]")).getText(), "Rifle Cruise Tour Fairing");
-        driver.quit();
     }
 
 }
