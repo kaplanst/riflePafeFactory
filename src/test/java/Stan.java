40904cbdfbb6bf64368d02f6df384dc6d2456aec
kaplanst <kaplanst@mail.ru>
2021-12-05 22:11:56 -0500

    Names added POM

--- src/test/java/Stan.java
+++ src/test/java/Stan.java
@@ -1,12 +1,7 @@
 import Utils.BaseTest;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class Stan extends BaseTest {
 
@@ -14,7 +9,6 @@ public class Stan extends BaseTest {
     void loginLinkTest() {  // Test case #TC-HD-001
         driver.findElement(By.xpath("//a[@id='ctl00_ctl00_NestedMaster_PageHeader_StoreHeaderRifle_H_LoginLink2']")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Account Sign In");
-        driver.quit();
     }
 
     @Test
@@ -24,7 +18,6 @@ public class Stan extends BaseTest {
         driver.findElement(By.xpath("//input[@id='ctl00_ctl00_NestedMaster_PageContent_LoginDialog1_Password']")).sendKeys("Qwerty1");
         driver.findElement(By.xpath("//input[@id='ctl00_ctl00_NestedMaster_PageContent_LoginDialog1_LoginButton']")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Account Sign In");
-        driver.quit();
     }
 
     @Test
@@ -34,16 +27,14 @@ public class Stan extends BaseTest {
         driver.findElement(By.xpath("//input[@id='ctl00_ctl00_NestedMaster_PageContent_LoginDialog1_Password']")).sendKeys("Qwerty1");
         driver.findElement(By.xpath("//input[@id='ctl00_ctl00_NestedMaster_PageContent_LoginDialog1_LoginButton']")).click();
         Assert.assertTrue(driver.findElement(By.xpath("//*[text()='The sign in information you provided was incorrect.']")).isDisplayed());
-        driver.quit();
     }
 
-      @Test
+    @Test
     void loginWithEmptyUsernameTest() {  // Test case #TC-HD-004
         driver.get("https://rifle.com/Login.aspx");
         driver.findElement(By.xpath("//input[@id='ctl00_ctl00_NestedMaster_PageContent_LoginDialog1_Password']")).sendKeys("Qwerty1");
         driver.findElement(By.xpath("//input[@id='ctl00_ctl00_NestedMaster_PageContent_LoginDialog1_LoginButton']")).click();
         Assert.assertTrue(driver.findElement(By.xpath("//*[text()='You must provide a user name.']")).isDisplayed());
-        driver.quit();
     }
 
 
