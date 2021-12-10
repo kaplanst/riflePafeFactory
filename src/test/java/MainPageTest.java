1aa9cc7032a10b6e01f4d17d4564bffb4f1b8938
Stanislav <kaplanst@mail.ru>
2021-12-10 17:48:54 -0500

    LoginPageTest Class created

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,2 +1,29 @@
-public class MainPageTest {
+import Utils.BaseTest;
+import model.LoginPage;
+import model.MainPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainPageTest extends BaseTest {
+
+    MainPage mainPage;
+
+    @BeforeMethod
+    void startTests(){
+        mainPage = PageFactory.initElements(driver, MainPage.class);
+    }
+
+    @Test
+    public void loginButtonTest() {  // Test case #TC-HD-001
+        mainPage.clickLoginButton();
+        Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Account Sign In");
+    }
+    @Test
+    public void accountButtonTest() {
+        mainPage.clickAccountButton();
+        Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(), "Account Sign In");
+    }
 }
