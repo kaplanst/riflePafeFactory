19f06503e70c068408373e291872af08ba1347f2
Stanislav <kaplanst@mail.ru>
2022-01-14 18:03:56 -0500

    Main Page added

--- src/test/java/tests/menuTests/FooterTest.java
+++ src/test/java/tests/menuTests/FooterTest.java
@@ -2,10 +2,14 @@ package tests.menuTests;
 
 import Utils.BaseTest;
 import model.menus.Footer;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class FooterTest extends BaseTest {
 
@@ -18,6 +22,11 @@ public class FooterTest extends BaseTest {
 
     @Test
     void headerFootersTest (){
+        List<WebElement> footerLinks = driver.findElements(By.xpath("//*[contains(@id, 'PageFooter_StoreFooterRifle_F')]"));
+        for (WebElement elem: footerLinks) {
+            System.out.print(elem.getText() + ", ");
+        }
+
         Assert.assertTrue(footer.main.isDisplayed() && footer.searchByMake.isDisplayed()
                     && footer.motorcycleProducts.isDisplayed() && footer.fourWheelOffroad.isDisplayed());
     }
